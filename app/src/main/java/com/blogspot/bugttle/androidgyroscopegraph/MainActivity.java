7768ef434d1aa7e9116b37db84c6579dc719f5ff
bugttle <bugttle@gmail.com>
2019-01-15 00:21:56 +0900

    Add MPAndroidChart library.

--- app/src/main/java/com/blogspot/bugttle/androidgyroscopegraph/MainActivity.java
+++ app/src/main/java/com/blogspot/bugttle/androidgyroscopegraph/MainActivity.java
@@ -1,5 +1,6 @@
 package com.blogspot.bugttle.androidgyroscopegraph;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -7,6 +8,11 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.Locale;
 
@@ -14,6 +20,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager mSensorManager;
     private Sensor mGyro;
     private TextView mTextView;
+    private LineChart mChart;
+
+    final String[] names = new String[]{ "x-values", "y-values", "z-values" };
+    final int[] colors = new int[]{ Color.RED, Color.GREEN, Color.BLUE };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         mTextView = findViewById(R.id.gyroTextView);
+        mChart = findViewById(R.id.chart);
+
+        mChart.setData(new LineData());
     }
 
     @Override
@@ -50,11 +63,39 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             final float sensorZ = sensorEvent.values[2];
             final String text = String.format(Locale.US, "Gyroscope: x=%f y=%f z=%f", sensorX, sensorY, sensorZ);
             mTextView.setText(text);
+
+            final LineData data = mChart.getLineData();
+            if (data != null) {
+                for (int i = 0; i < 3; ++i) {
+                    ILineDataSet set = data.getDataSetByIndex(i);
+                    if (set == null) {
+                        set = createSet(names[i], colors[i]);
+                        data.addDataSet(set);
+                    }
+
+                    data.addEntry(new Entry(set.getEntryCount(), sensorEvent.values[i]), i);
+                    data.notifyDataChanged();
+                }
+
+                mChart.notifyDataSetChanged();
+                mChart.setVisibleXRangeMaximum(50);
+                mChart.moveViewToX(data.getEntryCount());
+            }
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    private LineDataSet createSet(String label, int color) {
+        LineDataSet set = new LineDataSet(null, label);
+        set.setLineWidth(2.5f); // 線の幅を指定
+        set.setColor(color); // 線の色を指定
+        set.setDrawCircles(false); // ポイントごとの円を表示しない
+        set.setDrawValues(false); // 値を表示しない
+
+        return set;
     }
 }
