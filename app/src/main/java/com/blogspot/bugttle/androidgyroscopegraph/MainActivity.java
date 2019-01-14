af830441aac6b22e60e7bf93c77e9dae40d709cd
bugttle <bugttle@gmail.com>
2019-01-15 00:30:50 +0900

    Add writing log logic.

--- app/src/main/java/com/blogspot/bugttle/androidgyroscopegraph/MainActivity.java
+++ app/src/main/java/com/blogspot/bugttle/androidgyroscopegraph/MainActivity.java
@@ -1,26 +1,43 @@
 package com.blogspot.bugttle.androidgyroscopegraph;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.os.PowerManager;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private final int REQUEST_PERMISSION = 1;
+
+    private boolean mFileWritablePermitted = false;
     private SensorManager mSensorManager;
     private Sensor mGyro;
     private TextView mTextView;
@@ -42,7 +59,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mChart.setData(new LineData());
 
         if (mGyro != null) {
-            mSensorManager.registerListener(this, mGyro, SensorManager.SENSOR_DELAY_UI);
+            // almost every 1 second
+            mSensorManager.registerListener(this, mGyro, 1 * 1000 * 1000);
         } else {
             mTextView.setText("Not Supported");
         }
@@ -53,6 +71,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
             intent.setData(Uri.parse("package:" + getPackageName()));
             startActivity(intent);
+        }
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermission();
+        } else {
+            mFileWritablePermitted = true;
         }
     }
 
@@ -101,6 +125,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 mChart.setVisibleXRangeMaximum(50);
                 mChart.moveViewToX(data.getEntryCount());
             }
+
+            if (mFileWritablePermitted) {
+                save(sensorX, sensorY, sensorZ);
+            }
         }
     }
 
@@ -117,5 +145,80 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         set.setDrawValues(false); // 値を表示しない
 
         return set;
+    }
+
+    void save(final float x, final float y, final float z) {
+        if (isExternalStorageWritable()) {
+            String text;
+            File file = new File(Environment.getExternalStorageDirectory().getPath() + "/gyroscope.log");
+
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+                 OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
+                 BufferedWriter bw = new BufferedWriter(outputStreamWriter);
+            ) {
+                // ex.) 2016-06-01T06:59:45+09:00
+                java.text.DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
+                text = String.format(Locale.US, "%s,%f,%f,%f", df.format(new Date()), x, y, z);
+                bw.write(text);
+                bw.newLine();
+                bw.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void checkPermission() {
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mFileWritablePermitted = true;
+        }
+        // 拒否していた場合
+        else {
+            requestLocationPermission();
+        }
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+
+        } else {
+            Toast toast = Toast.makeText(this, "アプリ実行に許可が必要です", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,},
+                    REQUEST_PERMISSION);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mFileWritablePermitted = true;
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this, "何もできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
     }
 }
